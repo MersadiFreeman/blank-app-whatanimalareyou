4ac0891499c111a8afa442787dd8e97afb7eb176
MersadiFreeman <mersadi.mitchum@gmail.com>
2025-04-13 09:24:28 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write("What Animal Are You?")
+st.title("What Animal Are You?")
+st.write("Take this personality quiz to find out what animal you are with specific personality traits! Have fun!")
