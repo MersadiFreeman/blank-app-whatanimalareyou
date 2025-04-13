3d0398483e7e6662dd6862a6eff557e97ec4686a
MersadiFreeman <mersadi.mitchum@gmail.com>
2025-04-13 10:21:26 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("What Animal Are You?")
-st.write("Take this personality quiz to find out what animal you are with specific personality traits! Have fun!")
+st.write(Red + "Take this personality quiz to find out what animal you are with specific personality traits! Have fun!")
