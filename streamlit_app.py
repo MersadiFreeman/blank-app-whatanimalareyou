e4b1466da09933b16cc83683921fa8bc4ada3d82
MersadiFreeman <mersadi.mitchum@gmail.com>
2025-04-13 09:15:15 -0700

    Update streamlit_app.py
    Adding Title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title("🎈 My new app")
-st.write(
+st.write("What Animal Are You?")
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
